62b8fee57b1ba36bcc905f64445f570e16d20090
c_melchor <cah.melchor@gmail.com>
2020-11-13 10:53:41 -0800

    fixed form schema

--- src/formSchema.js
+++ src/formSchema.js
@@ -2,13 +2,16 @@ import * as yup from "yup";
 
 const formSchema = yup.object().shape({
   // .required() takes an error message
-  name: yup.string().required("Name is a required field"),
+  name: yup
+    .string()
+    .min(2, "must be at least 2 characters")
+    .required(),
   email: yup
     .string()
     .email()
     .required("Please enter an e-mail address"),
-  phone: yup.string().required(),
-  special: yup.string().required(),
+  phone: yup.string().required("Please enter a valid phone number"),
+  special: yup.string(),
   extraCheese: yup.boolean(),
   mushrooms: yup.boolean(),
   sausage: yup.boolean(),
